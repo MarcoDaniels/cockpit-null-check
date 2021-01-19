138fbd7afd61fe0b30e92f802242094ee3f52a28
Marco Martins <marcodanielmartins@gmail.com>
2021-01-19 10:01:23 +0100

    Simplify field type check

--- bootstrap.php
+++ bootstrap.php
@@ -19,20 +19,21 @@ $this->on('collections.save.after', function ($name, &$entry) {
 
 function handleEntry(&$collectionEntry, $collectionFields) {
     foreach ($collectionFields as $field) {
-        if (is_string($collectionEntry[$field['name']]) && strlen(trim($collectionEntry[$field['name']])) === 0) {
-            $collectionEntry[$field['name']] = null;
-        } else {
-            switch ($field['type']) {
-                case 'boolean':
-                    $collectionEntry[$field['name']] = is_bool($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : false;
-                    break;
-                case 'number':
-                    $collectionEntry[$field['name']] = is_numeric($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : 0;
-                    break;
-                default:
-                    $collectionEntry[$field['name']] = !empty($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : null;
-                    break;
-            }
+        switch ($field['type']) {
+            case 'string':
+            case 'text':
+                $isNonEmptyString = is_string($collectionEntry[$field['name']]) && strlen(trim($collectionEntry[$field['name']])) !== 0;
+                $collectionEntry[$field['name']] = $isNonEmptyString ? $collectionEntry[$field['name']] : null;
+                break;
+            case 'boolean':
+                $collectionEntry[$field['name']] = is_bool($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : false;
+                break;
+            case 'number':
+                $collectionEntry[$field['name']] = is_numeric($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : 0;
+                break;
+            default:
+                $collectionEntry[$field['name']] = !empty($collectionEntry[$field['name']]) ? $collectionEntry[$field['name']] : null;
+                break;
         }
     }
 }
